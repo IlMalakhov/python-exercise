9886bcd8f17186c4f7879520fa3ac55547449e90
Ilya <st090885@student.spbu.ru>
2024-11-13 16:08:00 +0000

    Add task2

--- .ipynb_checkpoints/task2-checkpoint.py
+++ .ipynb_checkpoints/task2-checkpoint.py
@@ -1,5 +1,17 @@
-"""This is my file.
-But put here your code please
-instead of this text
-and send me a pull request
-so I will be able to review your code"""
+def is_power(a, b):
+    """
+    Checks if a is a power of b.
+    """
+    # Define when to stop
+    if a == 1:
+        return True
+    # In this case we know that a cannot be a power of b
+    if a < b or a % b !=0:
+        return False
+    
+    # Recursively call to this function
+    return is_power(a // b, b)
+
+# Test
+print(is_power(16, 2))
+print(is_power(12,2))
